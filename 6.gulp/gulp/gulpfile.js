0b0ff8733eeb2b57640eb813779148c75ac0a3a9
wscats <2587575267@qq.com>
2018-05-23 11:27:03 +0800

    day3

--- 6.gulp/gulp/gulpfile.js
+++ 6.gulp/gulp/gulpfile.js
@@ -1,12 +1,9 @@
 var gulp = require("gulp");
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
-//es6转es6
-const babel = require('gulp-babel');
+var babel = require('gulp-babel');
 var refresh = require('gulp-refresh');
-
 var jade = require('gulp-jade');
-//定义任务
 //定义任务 sass任务
 gulp.task("sass", function() {
 	//导入文件
@@ -30,30 +27,6 @@ gulp.task("minifyjs", function() {
 		//导出文件
 		.pipe(gulp.dest("project/dist"))
 })
-//定义任务
-/*gulp.task("jade", function() {
-	var YOUR_LOCALS = {};
-	//导入文件
-	gulp.src("./project/jade/index.jade")
-		//转代码
-		.pipe(jade({
-			locals: YOUR_LOCALS
-		}))
-		//导出文件
-		.pipe(gulp.dest("./project/dist"))
-})*/
-
-//监听文件 文件改动时候触发任务
-/*gulp.task('watch', () => {
-	refresh.listen({
-		host:"localhost",
-		port:12345,
-		basePath:"./project/dist",
-		
-	})
-	gulp.watch(['project/sass/*.scss', ], ['sass']);
-	gulp.watch('project/js/*.js', ['minifyjs']);
-})*/
 
 var browserSync = require('browser-sync');
 gulp.task('myserver', () => {
@@ -70,7 +43,6 @@ gulp.task('myserver', () => {
 	// 监听sass文件修改
 	gulp.watch('project/sass/*.scss', ['sass']);
 	gulp.watch('project/js/*.js', ['minifyjs']);
-	//gulp.watch('project/jade/*.jade', ['jade']);
 });
 
 //gulp命令时候 敲命令时候触发任务
