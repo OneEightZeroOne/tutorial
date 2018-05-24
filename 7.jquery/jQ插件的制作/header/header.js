e24142b9f8972edef8208c2a2c80621dcd2ad2ee
wscats <2587575267@qq.com>
2018-05-24 11:18:47 +0800

    day4

--- 7.jquery/jQ插件的制作/header/header.js
+++ 7.jquery/jQ插件的制作/header/header.js
@@ -1,28 +1,31 @@
-jQuery.fn.extend({
-	
-	//js
-	xheader: function(option) {
-		console.log(this);
-		return this.each(function() {
-			//html
-			$.ajax({
-				type: "get",
-				url: "header/header.html",
-				success: function(data) {
-					$(this).html(data);
-					$("header").text(option.title);
-				}.bind(this)
-			});
+;(function(jQuery) {
+	var b = 2;
+	jQuery.fn.extend({
 
-			//css
-			$.ajax({
-				type: "get",
-				url: "header/header.css",
-				success: function(data) {
-					$("head style").append(data)
-				}.bind(this)
-			});
+		//js
+		xheader: function(option) {
+			console.log(this);
+			return this.each(function() {
+				//html
+				$.ajax({
+					type: "get",
+					url: "header/header.html",
+					success: function(data) {
+						$(this).html(data);
+						$(this).find("header").text(option.title);
+					}.bind(this)
+				});
 
-		});
-	}
-});
+				//css
+				$.ajax({
+					type: "get",
+					url: "header/header.css",
+					success: function(data) {
+						$("head style").append(data)
+					}.bind(this)
+				});
+
+			});
+		}
+	});
+})(jQuery);
--- 7.jquery/jQ插件的制作/nav/nav.js
+++ 7.jquery/jQ插件的制作/nav/nav.js
@@ -1,28 +1,31 @@
-jQuery.fn.extend({
-	xnav: function(option) {
-		console.log(this);
-		return this.each(function() {
-			//html
-			$.ajax({
-				type: "get",
-				url: "nav/nav.html",
-				success: function(data) {
-					$(this).html(data);
-					
-					$(".dropdown-toggle").click(function(){
-						$(".dropdown-menu").toggle(1000,"linear");
-					})
-				}.bind(this)
-			});
+;(function(jQuery) {
+	var b = 1;
+	jQuery.fn.extend({
+		xnav: function(option) {
+			return this.each(function() {
+				//html
+				$.ajax({
+					type: "get",
+					url: "nav/nav.html",
+					cache: true,
+					success: function(data) {
+						console.log(this);
+						$(this).html(data);
+						$(this).find(".dropdown-toggle").click(function() {
+							$(this).find(".dropdown-menu").toggle(1000, "linear");
+						}.bind(this))
+					}.bind(this)
+				});
 
-			//css
-			/*$.ajax({
-				type: "get",
-				url: "nav/nav.css",
-				success: function(data) {
-					$("head style").append(data)
-				}.bind(this)
-			});*/
-		});
-	}
-});
+				//css
+				/*$.ajax({
+					type: "get",
+					url: "nav/nav.css",
+					success: function(data) {
+						$("head style").append(data)
+					}.bind(this)
+				});*/
+			});
+		}
+	});
+})(jQuery);
